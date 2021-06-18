c661ed05afc90153c98a75c765ee95bb348c9d5d
bishwaskharel <bishukharel98@gmail.com>
2021-06-18 19:52:30 +0545

    Services Section in About us page

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,11 +1,13 @@
 import React from "react";
 // Import About Components
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
 
 const AboutUs = () => {
   return (
     <div>
       <AboutSection />
+      <ServicesSection />
     </div>
   );
 };
